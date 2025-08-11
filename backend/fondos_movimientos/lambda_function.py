93ebfbf92c04bc5d1289d06a0c78f7194884452f
AlissonGCumplimos <dayana.garay.lozano@gmail.com>
2025-08-11 02:53:43 -0500

    problema de cors

--- backend/fondos_movimientos/lambda_function.py
+++ backend/fondos_movimientos/lambda_function.py
@@ -1,8 +1,8 @@
+"""controlador"""
 import json
 
 from utils import ExceptionPeticion, ExceptionCustom, JSONEncoder
 from peticion_get import peticion_get
-
 
 def lambda_handler(event, context):
     """controlador del lambda"""
@@ -24,14 +24,24 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "body": json.dumps(res, cls=JSONEncoder),
+            "headers": {
+                "Access-Control-Allow-Origin": "*", 
+                "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+            }
         }
     except ExceptionPeticion as ex:
         res["codigo"]= 400
         res["msg"]= ex.msg
         res["msg_context"]= f"{ex}"
         return {
-            "statusCode": 404,
+            "statusCode": 400,
             "body": json.dumps(res, cls=JSONEncoder),
+            "headers": {
+                "Access-Control-Allow-Origin": "*", 
+                "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+            }
         }
     except ExceptionCustom as ex:
         res["codigo"]= 404
@@ -40,11 +50,21 @@ def lambda_handler(event, context):
         return {
             "statusCode": 404,
             "body": json.dumps(res, cls=JSONEncoder),
+            "headers": {
+                "Access-Control-Allow-Origin": "*", 
+                "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+            }
         }
     except Exception as ex:
-        res["codigo"]= 500
+        res["codigo"]= 400
         res["msg_context"]= f"{ex}"
         return {
-            "statusCode": 500,
+            "statusCode": 400,
             "body": json.dumps(res, cls=JSONEncoder),
+            "headers": {
+                "Access-Control-Allow-Origin": "*", 
+                "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+                "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+            }
         }
