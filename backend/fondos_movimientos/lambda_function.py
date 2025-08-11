dfe2fc65431f8206043ff326bdd7e4165f33d86f
AlissonGCumplimos <dayana.garay.lozano@gmail.com>
2025-08-11 02:36:48 -0500

    arreglar movimientos

---
+++ backend/fondos_movimientos/lambda_function.py
@@ -0,0 +1,50 @@
+import json
+
+from utils import ExceptionPeticion, ExceptionCustom, JSONEncoder
+from peticion_get import peticion_get
+
+
+def lambda_handler(event, context):
+    """controlador del lambda"""
+    res= {
+        "status":False,
+        "codigo":400,
+        "msg":"Error de aplicación",
+        "msg_context":"Error de aplicación",
+        "obj":{}
+    }
+
+    try:
+        method= event.get("httpMethod")
+        if method == "GET":
+            params= event.get("queryStringParameters")
+            res= peticion_get(params)
+        else:
+            raise ExceptionCustom("<<RaiseCustom>> No se encontro metodo")
+        return {
+            "statusCode": 200,
+            "body": json.dumps(res, cls=JSONEncoder),
+        }
+    except ExceptionPeticion as ex:
+        res["codigo"]= 400
+        res["msg"]= ex.msg
+        res["msg_context"]= f"{ex}"
+        return {
+            "statusCode": 404,
+            "body": json.dumps(res, cls=JSONEncoder),
+        }
+    except ExceptionCustom as ex:
+        res["codigo"]= 404
+        res["msg"]= "Metodo no encontrado"
+        res["msg_context"]= f"{ex}"
+        return {
+            "statusCode": 404,
+            "body": json.dumps(res, cls=JSONEncoder),
+        }
+    except Exception as ex:
+        res["codigo"]= 500
+        res["msg_context"]= f"{ex}"
+        return {
+            "statusCode": 500,
+            "body": json.dumps(res, cls=JSONEncoder),
+        }
---
+++ backend/fondos_movimientos/peticion_get.py
@@ -0,0 +1,102 @@
+"""peticion post"""
+import os
+from typing import Dict, Any
+from schema import Use, And, Schema, Optional as OptionalSchema
+
+from utils import ExceptionPeticion
+from lib.dynamo_lib import DynamoDBHandler, parse_format_dynamo
+
+def peticion_get(params:Any)->Dict:
+    """peticion para listar tus productos"""
+    if params is None:
+        raise ExceptionPeticion(
+            "<<Raise Custom>> params vacio",
+            "Datos de entrada invalidos"
+        )
+
+    try:
+        schema_params= Schema(
+            Schema({
+                "id_user": And(
+                    Use(int),
+                    error= "'id_user' debe ser numerico"
+                ),
+                OptionalSchema("fecha"): And(
+                    Use(str),
+                    error= "'Nombre' debe ser string"
+                ),
+                OptionalSchema("limit", default= 10): And(
+                    Use(int),
+                    error= "'limit' debe ser numerico"
+                ),
+                OptionalSchema("id_movimiento_start"): And(
+                    Use(int),
+                    error= "'id_movimiento_start' debe ser numerico"
+                ),
+                OptionalSchema("page", default= 1): And(
+                    Use(int),
+                    error= "'page' debe ser numerico"
+                )
+            })
+        )
+        params:Dict= schema_params.validate(params)
+    except Exception as ex:
+        raise ExceptionPeticion(
+            f"<<SchemaError>> {ex}",
+            "Datos de entrada invalidos"
+        ) from ex
+
+    instance_dynamo= DynamoDBHandler(
+        os.getenv("AWS_REGION_SERVICES")
+    )
+
+    #secuencia buscar movimientos
+    start_key= None
+    if params["page"] >= 1 and params.get("id_movimiento_start") is not None:
+        start_key=parse_format_dynamo({
+            "id": str(params["id_movimiento_start"]),
+            "id_user": params["id_user"]
+        })
+    list_movimientos_, last_key= instance_dynamo.get_item_paginated(
+        os.getenv("TABLE_FONDOS_MOVIMIENTOS"),
+        {"id_user": params["id_user"]},
+        params["limit"],
+        start_key= start_key,
+        index_name= "id_user-index"
+    )
+    id_movimiento_start= None
+    if len(list_movimientos_) > 0 and params["page"] > 1:
+        indmov:Dict= list_movimientos_[0]
+        id_movimiento_start= indmov["id"]
+    id_movimiento_last= None
+    if last_key is not None:
+        id_movimiento_last= last_key.get("id")
+
+
+    list_movimientos= []
+    for indmov_ in list_movimientos_:
+        indmov:Dict= indmov_
+        producto:Dict= indmov["producto"]
+        list_movimientos.append({
+            "id_movimiento": indmov["id"],
+            "producto":{
+                "id":producto["id"],
+                "nombre":producto["Nombre"],
+                "categoria":producto["Categoria"],
+            },
+            "fecha": indmov["fecha"],
+            "valor": indmov["valor"]
+        })
+
+    res= {
+        "status":True,
+        "codigo":200,
+        "msg":"Consulta exitosa",
+        "msg_context":"",
+        "obj":{
+            "movimientos": list_movimientos,
+            "id_movimiento_start": id_movimiento_start,
+            "id_movimiento_last": id_movimiento_last
+        }
+    }
+    return res
